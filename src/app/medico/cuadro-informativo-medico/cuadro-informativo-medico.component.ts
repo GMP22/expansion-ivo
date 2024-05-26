c7ad8fce46bb5b0e26d15b56e4b6e68daaafd122
GMP22 <gemepe28@gmail.com>
2024-05-26 19:35:55 +0200

    WIP de Pedidos y modificacion de componente de Cuadro Informativo

--- src/app/medico/cuadro-informativo-medico/cuadro-informativo-medico.component.ts
+++ src/app/medico/cuadro-informativo-medico/cuadro-informativo-medico.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { faCalendar } from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faUserPlus } from '@fortawesome/free-solid-svg-icons';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
+import { faTruckFast } from '@fortawesome/free-solid-svg-icons';
+import { Input } from '@angular/core';
 
 @Component({
   selector: 'app-cuadro-informativo-medico',
@@ -6,5 +12,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./cuadro-informativo-medico.component.css']
 })
 export class CuadroInformativoMedicoComponent {
+  faTruckFast = faTruckFast;
+
+  @Input() numeroDeCosas!:number;
+  @Input() TituloDeCuadro!:string;
 
 }
